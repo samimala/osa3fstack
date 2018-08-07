14f76b4306c45b35ecec8a1a1937f8639dabdc7d
Sami Mäläskä <epsilon@welho.com>
2018-08-07 19:57:33 +0300

    Teht4

--- index.js
+++ index.js
@@ -44,6 +44,13 @@ app.get('/api/persons/:id', (req, res) => {
    }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    catalogue = catalogue.filter(person => person.id !== id)
+    res.json(person)
+    res.status(204).end()
+ })
+ 
 app.get('/api/persons', (req, res) => {
     res.json(catalogue)
  })
