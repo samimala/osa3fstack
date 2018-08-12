c0a28a6b44012a25b5f2afc3461b98d573ea8fab
Sami Mäläskä <epsilon@welho.com>
2018-08-12 23:29:32 +0300

    DB list

---
+++ models/person.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const url = 'mongodb://fstakkaaja:salaTstac3@ds219532.mlab.com:19532/fstackkanta'
+
+mongoose.connect(url, { useNewUrlParser: true })
+
+
+const Person = mongoose.model('Person', {
+    name: String,
+    number: String,
+})
+
+module.exports = Person
