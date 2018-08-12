66cab64909acb0ccc48a2ca49cfa56b996735861
Sami Mäläskä <epsilon@welho.com>
2018-08-13 00:36:44 +0300

    Teht 13

--- models/person.js
+++ models/person.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const url = 'mongodb://fstakkaaja:salaTstac3@ds219532.mlab.com:19532/fstackkanta'
+const url = 'mongodb://fstakkaaja:<salasana>@ds219532.mlab.com:19532/fstackkanta'
 
 mongoose.connect(url, { useNewUrlParser: true })
 
