ab86d98df934584083bc86d86b6d6b9dfba82ac9
Sami Mäläskä <epsilon@welho.com>
2018-08-14 00:01:48 +0300

    DBmuutos

--- models/person.js
+++ models/person.js
@@ -11,7 +11,7 @@ const personSchema = new mongoose.Schema({
 })
 
 personSchema.statics.format = function(person) {
-    return { name: person.name, number: person.number } 
+    return { name: person.name, number: person.number, id: person.id } 
 };
 
 
