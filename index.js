8963297d97765625288f5bdea56d432f038321b0
Sami Mäläskä <epsilon@welho.com>
2018-08-20 20:23:30 +0300

    Teht22 eslint

--- index.js
+++ index.js
@@ -26,39 +26,39 @@ app.get('/info', (req, res) => {
       res.send('<div>Counting produce error: ' + error + '</div>')
     })
 })
- 
+
 app.get('/api/persons/:id', (req, res) => {
   const id = req.params.id
-   
+
   Person
     .findById(id)
     .then(person => {
       res.json(Person.format(person))
     })
-    .catch(error =>res.status(404).send({error: error})
+    .catch(error => res.status(404).send({ error: error })
     )
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    Person
-      .findByIdAndRemove(req.params.id)
-      .then(result=>res.status(204).end())
-      .catch(error=>res.status(400).send({error: 'Error in id'}))
- })
+  Person
+    .findByIdAndRemove(req.params.id)
+    .then(res.status(204).end())
+    .catch(res.status(400).send({ error: 'Error in id' }))
+})
 
 
 app.put('/api/persons/:id', (req, res) => {
   const updateperson = req.body
   console.log('Got request: ', req)
   Person
-    .findByIdAndUpdate(req.params.id, updateperson, {new: true})
+    .findByIdAndUpdate(req.params.id, updateperson, { new: true })
     .then(person => {
-      console.log('putted: ', Person.format(person)) 
+      console.log('putted: ', Person.format(person))
       res.json(Person.format(person))
     })
     .catch(error => {
       console.log(error)
-      res.status(400).send({error: 'Error in id'})
+      res.status(400).send({ error: 'Error in id' })
     })
 })
 
@@ -70,10 +70,10 @@ app.post('/api/persons/', (req, res) => {
   
   // Tarkista, että data sisältää nimen ja numeron
   if (newperson.name === undefined) {
-    return res.status(400).json({error: 'Name missing'})  
+    return res.status(400).json({ error: 'Name missing' })
   }
   if (newperson.number === undefined) {
-    return res.status(400).json({error: 'Number missing'})  
+    return res.status(400).json({ error: 'Number missing' })
   }
 
   const addNewPerson = (person) => {
@@ -95,33 +95,32 @@ app.post('/api/persons/', (req, res) => {
   }
 
   Person
-  .findOne({name: newperson.name})
-  .then(person => {
-    console.log('person already exists ', Person.format(person))
-    res.status(404).json({error: 'name exists'})
-  })
-  .catch(error => {
-    console.log('Adding person')
-    addNewPerson(newperson)
-  })
+    .findOne({ name: newperson.name })
+    .then(person => {
+      console.log('person already exists ', Person.format(person))
+      res.status(404).json({error: 'name exists'})
+    })
+    .catch(error => {
+      console.log('Adding person')
+      addNewPerson(newperson)
+    })
 })
 
 app.get('/api/persons', (req, res) => {
   console.log('Finding persons')
   Person
-      .find({})
-      .then(persons => {
-          console.log('Persons found in DB')
-          console.log(persons.map(Person.format))
-          res.json(persons.map(Person.format))
-      })
-      .catch(error => {
-        console.log('Get persons: ', error)
-      }) 
- })
- 
+    .find({})
+    .then(persons => {
+      console.log('Persons found in DB')
+      console.log(persons.map(Person.format))
+      res.json(persons.map(Person.format))
+    })
+    .catch(error => {
+      console.log('Get persons: ', error)
+    })
+})
 
 const port=process.env.PORT || 3001
 app.listen(port, ()  => {
-  console.log('running')  
+  console.log('running')
 })
